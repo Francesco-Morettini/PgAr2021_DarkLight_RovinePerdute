e0f5834d197d2ad4554ff99f49472ef02cf2fe25
desikalakar786 <81826196+desikalakar786@users.noreply.github.com>
2021-05-25 17:29:02 +0200

    Kamal

--- RovinePerdute/src/Citta.java
+++ RovinePerdute/src/Citta.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class Citta {
+    private int id;
+    private String nome;
+    private Posizione posizione;
+    private ArrayList<Integer> link;
+
+    public Citta(int id, String nome, Posizione posizione, ArrayList<Integer> link) {
+        this.id = id;
+        this.nome = nome;
+        this.posizione = posizione;
+        this.link = link;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Posizione getPosizione() {
+        return posizione;
+    }
+
+    public void setPosizione(Posizione posizione) {
+        this.posizione = posizione;
+    }
+
+    public ArrayList<Integer> getLink() {
+        return link;
+    }
+
+    public void setLink(ArrayList<Integer> link) {
+        this.link = link;
+    }
+
+
 }
--- RovinePerdute/src/Percorso.java
+++ RovinePerdute/src/Percorso.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Percorso {
+    private ArrayList<Citta> citta;
+    private ArrayList<Citta> rotta;
+
+    public Percorso() {
+    }
+
+    public ArrayList<Citta> getCitta() {
+        return citta;
+    }
+
+    public void setCitta(ArrayList<Citta> citta) {
+        this.citta = citta;
+    }
+
+    public ArrayList<Citta> getRotta() {
+        return rotta;
+    }
+
+    public void setRotta(ArrayList<Citta> rotta) {
+        this.rotta = rotta;
+    }
 }
