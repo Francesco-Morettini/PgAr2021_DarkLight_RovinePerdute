5ce89fa72fd9743eabb5c4c1e0e29eccf7004ab2
Francesco-Morettini <frcmorettini.01@gmail.com>
2021-05-25 17:34:22 +0200

    Francesco

--- RovinePerdute/src/GestioneFile.java
+++ RovinePerdute/src/GestioneFile.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class GestioneFile {
+
+    private ArrayList<Citta> citta;
+
+    /*public void leggiFile(){
+
+    }*/
+
+    /*public void scriviFile(ArrayList<Squadra> squadre){
+
+    }*/
+
+    /*public ArrayList<Citta> getCitta(){
+
+    }*/
+
 }
--- RovinePerdute/src/Squadra.java
+++ RovinePerdute/src/Squadra.java
@@ -1,2 +1,45 @@
 public class Squadra {
+
+    private String nome;
+    private int tipoVeicolo;
+    private double carburanteConsumato;
+    private Percorso percorso;
+
+    public Squadra(String nome, int tipoVeicolo) {
+        setNome(nome);
+        setTipoVeicolo(tipoVeicolo);
+        percorso = new Percorso();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTipoVeicolo() {
+        return tipoVeicolo;
+    }
+
+    public void setTipoVeicolo(int tipoVeicolo) {
+        this.tipoVeicolo = tipoVeicolo;
+    }
+
+    public double getCarburanteConsumato() {
+        return carburanteConsumato;
+    }
+
+    public void setCarburanteConsumato(double carburanteConsumato) {
+        this.carburanteConsumato = carburanteConsumato;
+    }
+
+    public Percorso getPercorso() {
+        return percorso;
+    }
+
+    public void setPercorso() {
+
+    }
 }
